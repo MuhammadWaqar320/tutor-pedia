c084548c9311a374e38cdec7de864a171c079379
Samad Saleem <samad.saleem@fusiontech.global>
2024-03-06 11:44:31 +0500

    changes pushed

--- client/src/app/dashboards/admin/page.tsx
+++ client/src/app/dashboards/admin/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 import React from "react";
 import DashboardLayout from "@/components/DashboardLayout";
-import { MenuListType } from "@/components/DashboardLeftSideBar";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import {studentDashboardMenuItem} from "@/utils/constant";
-
 
 const StudentDashboard = () => {
 
